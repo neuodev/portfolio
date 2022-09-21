3a28d4fa6ce8feb9c3edf3ab715c4bc81e157aaa
Ahmed Ibrahim <ib153507@gmail.com>
2022-09-21 17:50:41 +0200

    Add custom button

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,32 +1,38 @@
 import React from "react";
 import Image from "next/image";
+import Button from "./common/Button";
 
 const Home = () => {
   return (
     <div className="h-screen w-screen bg-[url('/images/hero.jpg')] relative overlay">
       <div className="relative z-10 flex items-center justify-center h-screen w-screen">
-      <div className="ring-8 ring-gray-600 rounded-full overflow-hidden mr-12">
-        <Image
-          src="/images/ahmed.jpg"
-          alt="Ahmed Ibrahim"
-          height={300}
-          width={300}
-        />
-      </div>
-      <div className="text-white max-w-lg">
-        <p className="uppercase text-lg mb-2">HI THERE!</p>
-        <h1 className="mt-2 mb-3 text-5xl font-bold uppercase">I&apos;m <span className="text-main">a web developer</span></h1>
-        <p className="mt-2 mb-3 leading-relaxed">
-          I&apos;m a Freelance UI/UX Designer and Developer based in London,
-          England. I strives to build immersive and beautiful web applications
-          through carefully crafted code and user-centric design.
-        </p>
-        <div>
-          <button>More about me</button>
-          <button>Portfolio</button>
+        <div className="ring-8 ring-gray-600 rounded-full overflow-hidden mr-12">
+          <Image
+            src="/images/ahmed.jpg"
+            alt="Ahmed Ibrahim"
+            height={300}
+            width={300}
+          />
         </div>
-      </div>
-    
+        <div className="text-white max-w-lg">
+          <p className="uppercase text-lg mb-2">HI THERE!</p>
+          <h1 className="mt-2 mb-3 text-5xl font-bold uppercase">
+            I&apos;m <span className="text-main">a web developer</span>
+          </h1>
+          <p className="mt-2 mb-6 leading-relaxed">
+            I&apos;m a Freelance UI/UX Designer and Developer based in London,
+            England. I strives to build immersive and beautiful web applications
+            through carefully crafted code and user-centric design.
+          </p>
+          <div>
+            <Button className="mr-4 w-52" variant="primary">
+              More about me
+            </Button>
+            <Button variant="secondary" className="w-52">
+              Portfolio
+            </Button>
+          </div>
+        </div>
       </div>
     </div>
   );
--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -1,8 +1,22 @@
 import type { NextPage } from "next";
 import React from "react";
 
-const Button: NextPage<{ text: string }> = ({ text }) => {
-  return <button>{text}</button>;
+const Button: NextPage<{
+  children: React.ReactNode;
+  className?: string;
+  variant?: "primary" | "secondary";
+}> = ({ children, className, variant }) => {
+  return (
+    <button
+      className={`px-8 py-3 ${
+        variant === "secondary"
+          ? "border border-main text-main"
+          : "bg-main text-white"
+      } uppercase rounded-3xl font-semibold ${className || ""}`}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
