ce921ba497ade76788e93d1d7472e09cf8d5e78b
Ahmed Ibrahim <ib153507@gmail.com>
2022-10-01 11:49:24 +0200

    Update section title & subtitle

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Element } from "react-scroll";
 import me from "../json/me.json";
+import SectionTitle from "./common/SectionTitle";
 import Project, { IProject } from "./Project";
 import ProjectDetails from "./ProjectDetails";
 
@@ -12,13 +13,11 @@ const Projects = () => {
       name="projects"
       className="text-gray-300 w-full h-screen snap-start"
     >
-      <div className="w-ful flex items-center justify-center flex-col py-16">
-        <h1 className="text-7xl uppercase font-bold">
-          My <span className="text-indigo-500">Projects</span>
-        </h1>
-        <p className="mt-5 flex items-center justify-center">
-          <span className="w-9 h-0.5 bg-indigo-500 inline-block"></span>
-          <span className="mx-4 inline-block uppercase">
+      <SectionTitle
+        prefix="My"
+        focus="Projects"
+        subtitle={
+          <>
             want to see more? checkout my{" "}
             <a
               className="text-indigo-500 hover:underline"
@@ -28,10 +27,9 @@ const Projects = () => {
             >
               GitHub
             </a>
-          </span>
-          <span className="w-9 h-0.5 bg-indigo-500 inline-block"></span>
-        </p>
-      </div>
+          </>
+        }
+      />
       <div className="container mx-auto">
         <div className="grid grid-cols-12 grid-rows-4 lg:grid-rows-6 max-w-screen-lg mx-auto gap-1 min-h-700 lg:min-h-700">
           {me.projects.map((project) => (
